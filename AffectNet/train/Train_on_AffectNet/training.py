f2c9353f35f2387225d00b625ec553df19d6e6ad
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2020-04-28 22:24:21 +0200

    Test version of training on AffectNet #2

--- AffectNet/train/Train_on_AffectNet/training.py
+++ AffectNet/train/Train_on_AffectNet/training.py
@@ -27,7 +27,7 @@ channels=3
 image_shape=(height, width, channels)
 
 # Model
-path_to_weights= r'C:\Users\Denis\Desktop\AffectNet\Resnet_model\model\resnet50_softmax_dim512\\weights.h5'
+path_to_weights= r'C:\Users\Dresvyanskiy\Desktop\Projects\DNN_emotion_recognition\Resnet_model\model\resnet50_softmax_dim512\weights.h5'
 
 model=model_AffectNet(input_dim=image_shape, path_to_weights=path_to_weights, trained=False)
 for i in range(len(model.layers)):
@@ -47,21 +47,22 @@ path_to_stats='stats/'
 if not os.path.exists(path_to_stats): os.mkdir(path_to_stats)
 best_result=100000
 # set up
-path_to_data=''
+path_to_data='C:\\Users\\Dresvyanskiy\\Desktop\\Projects\\DNN_emotion_recognition\mini_batches\\'
 train_data_prefix='train_data_batch'
 train_labels_prefix='train_labels_batch'
 validation_data_prefix='validation_data'
 validation_labels_prefix='validation_labels'
 # check how much batches we have and then create a list of num batches
 files=np.array(os.listdir(path_to_data))
-files=files[train_data_prefix in files]
+mask=np.array([train_data_prefix in x for x in files])
+files=files[mask]
 num_batches=files.shape[0]
 # create a list of paths to batches
 train_data_batches=[]
 train_labels_batches=[]
 for i in range(num_batches):
-    train_data_batches.append(path_to_data+train_data_prefix+str(i)+'.npy')
-    train_labels_batches.append(path_to_data+train_labels_prefix+str(i)+'.csv')
+    train_data_batches.append(path_to_data+train_data_prefix+'_'+str(i)+'.npy')
+    train_labels_batches.append(path_to_data+train_labels_prefix+'_'+str(i)+'.csv')
 train_data_batches=np.array(train_data_batches)
 train_labels_batches=np.array(train_labels_batches)
 # training process
@@ -76,7 +77,7 @@ for epoch in range(epochs):
         data=data.astype('float32')
         permutations = np.random.permutation(data.shape[0])
         data, labels= data[permutations], labels.iloc[permutations]
-        history=model.fit(data, labels[label_type],batch_size=batch_size, epochs=1, verbose=2)
+        history=model.fit(data, labels[label_type],batch_size=batch_size, epochs=1, verbose=1, use_multiprocessing=True)
         train_loss.append(history.history['loss'][0])
         if step%int(num_batches/4)==0:
             val_data, val_labels=load_data_and_preprocess(path_to_data+validation_data_prefix+'.npy', path_to_data+validation_labels_prefix+'.csv')
@@ -91,15 +92,4 @@ for epoch in range(epochs):
             del val_data
             del val_labels
             gc.collect()
-
-
-
-
-
-
-
-
-
-
-
 
