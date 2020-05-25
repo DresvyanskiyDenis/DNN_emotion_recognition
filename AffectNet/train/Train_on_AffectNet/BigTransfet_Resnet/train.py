49270a96aa5c7160dd29e1aabbe2ab75ab7c9e47
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2020-05-25 13:32:40 +0200

    Stable version for colab

--- AffectNet/train/Train_on_AffectNet/BigTransfet_Resnet/train.py
+++ AffectNet/train/Train_on_AffectNet/BigTransfet_Resnet/train.py
@@ -36,7 +36,7 @@ FAU_shape=(17,)
 
 # Train params
 lr=0.003
-batch_size=256
+batch_size=64
 epochs=10
 verbose=2
 label_type='arousal'
@@ -67,6 +67,7 @@ for i in range(num_batches):
     train_FAUs_batches.append(path_to_data+train_FAU_prefix+'_'+str(i)+'.npy')
     train_labels_batches.append(path_to_data+train_labels_prefix+'_'+str(i)+'.csv')
 train_data_batches=np.array(train_data_batches)
+train_FAUs_batches=np.array(train_FAUs_batches)
 train_labels_batches=np.array(train_labels_batches)
 
 # Model
@@ -78,7 +79,7 @@ print(model.summary())
 # training process
 
 for epoch in range(epochs):
-    if (epochs)%2==0:
+    if (epochs)%1==0:
         lr=lr/10.
         optimizer = tf.keras.optimizers.SGD(learning_rate=lr, momentum=0.9)
         model.compile(optimizer=optimizer, loss='mse')
@@ -86,19 +87,22 @@ for epoch in range(epochs):
         batch_size=int(batch_size/2)
     permutations=np.random.permutation(train_data_batches.shape[0])
     train_data_batches=train_data_batches[permutations]
+    train_FAUs_batches=train_FAUs_batches[permutations]
     train_labels_batches=train_labels_batches[permutations]
     for step in range(num_batches):
         print('epoch:', epoch, '  sub-epoch:',step)
+        #print(train_data_batches[step],train_FAUs_batches[step], train_labels_batches[step])
         data, FAU, labels=load_data_and_preprocess(train_data_batches[step],train_FAUs_batches[step], train_labels_batches[step])
+        #print(data.shape, FAU.shape, labels.shape)
         data=data.astype('float32')
         permutations = np.random.permutation(data.shape[0])
         data, FAU, labels= data[permutations], FAU[permutations], labels.iloc[permutations]
         history=model.fit([data, FAU], labels[label_type].values,batch_size=batch_size, epochs=1, verbose=1,
                           use_multiprocessing=True, callbacks=[MyCustomCallback()])
         train_loss.append(history.history['loss'][0])
-        if step%int(num_batches/4)==0:
+        if step%int(num_batches/6)==0:
             val_data,val_FAUs, val_labels=load_data_and_preprocess(path_to_data+validation_data_prefix+'.npy',path_to_data+ validation_FAU_prefix+'.npy', path_to_data+validation_labels_prefix+'.csv')
-            results = model.evaluate(x=val_data, y=val_labels[label_type].values, verbose=2, batch_size=batch_size)
+            results = model.evaluate(x=[val_data, val_FAUs], y=val_labels[label_type].values, verbose=2, batch_size=batch_size)
             val_loss.append(results)
             if results < best_result:
                 best_result = results
