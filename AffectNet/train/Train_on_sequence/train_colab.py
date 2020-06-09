f8108a5cd27542642802f7334e1a966ee7b22102
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2020-06-09 23:12:33 +0200

    function to evaluate mse of database was added. Test version

--- AffectNet/train/Train_on_sequence/train_colab.py
+++ AffectNet/train/Train_on_sequence/train_colab.py
@@ -2,11 +2,35 @@ import gc
 import os
 import pandas as pd
 import numpy as np
+from keras import backend as K
+from sklearn.metrics import mean_squared_error
 
 from AffectNet.train.Train_on_AffectNet.BigTransfet_Resnet.model import create_sequence_model
+global AffectNet_embeddings_output_shape
 
+def CCC_loss_tf(y_true, y_pred):
+    """
+    This function calculates loss based on concordance correlation coefficient of two series: 'ser1' and 'ser2'
+    TensorFlow methods are used
+    """
 
-def train_generator(paths):
+    y_pred = K.squeeze(y_pred, axis=-1)
+    y_true = K.squeeze(y_true, axis=-1)
+
+    y_true_mean = K.mean(y_true, axis=-1, keepdims=True)
+    y_pred_mean = K.mean(y_pred, axis=-1, keepdims=True)
+
+    y_true_var = K.mean(K.square(y_true-y_true_mean), axis=-1, keepdims=True)
+    y_pred_var = K.mean(K.square(y_pred-y_pred_mean), axis=-1, keepdims=True)
+
+    cov = (y_true-y_true_mean)*(y_pred-y_pred_mean)
+
+    ccc = K.constant(2.) * cov / (y_true_var + y_pred_var + K.square(y_true_mean - y_pred_mean) + K.common.epsilon())
+    ccc_loss = K.constant(1.) - ccc
+
+    return ccc_loss
+
+def train_generator(paths, need_permutation=True):
     paths_to_data = np.array([], dtype='str')
     paths_to_labels = np.array([], dtype='str')
     for database_path in paths:
@@ -16,11 +40,12 @@ def train_generator(paths):
         paths_to_data = np.append(paths_to_data, data_files)
         paths_to_labels = np.append(paths_to_labels, labels_files)
     # now you got all paths to data and labels. Shuffle it!
-    permutation = np.random.permutation(paths_to_data.shape[0])
-    paths_to_data = paths_to_data[permutation]
-    paths_to_labels = paths_to_labels[permutation]
+    if need_permutation:
+        permutation = np.random.permutation(paths_to_data.shape[0])
+        paths_to_data = paths_to_data[permutation]
+        paths_to_labels = paths_to_labels[permutation]
     # Let's read files and give it to output
-    amount_parts = 3
+    amount_parts = 1
     for part_idx in range(0, paths_to_data.shape[0], amount_parts):
         amount_in_current_part = 0
         data = None
@@ -39,10 +64,32 @@ def train_generator(paths):
             amount_in_current_part += 1
         yield data, labels
 
+def make_predictions_on_database(path_to_database, model, label_types):
+    gen = train_generator([path_to_database], need_permutation=False)
+    real_labels = []
+    for mini_batch in gen:
+        data, label = mini_batch
+        data, mask_for_data, _ = preprocess_data_and_labels_for_train(data, label, label_types)
+        predicted_labels = model.predict([data, mask_for_data])
+        for idx_window in range(predicted_labels.shape[0]):
+            for i in range(len(labels_type)):
+                label[idx_window]['prediction_' + labels_type[i]] = predicted_labels[i]
+        real_labels += label
+    return real_labels
 
-def evaluate_mse_on_database():
-    # TODO: develop the function
-    pass
+def concatenate_all_dataframes_in_list(list_of_dataframes):
+    full_dataframe=list_of_dataframes[0]
+    for i in range(1, len(list_of_dataframes)):
+        full_dataframe=pd.concat([full_dataframe, list_of_dataframes[i]])
+    return full_dataframe
+
+
+def evaluate_mse_on_database(path_to_database, model, label_types):
+    list_of_dataframe_labels=make_predictions_on_database(path_to_database, model, label_types)
+    concatenated_labels=concatenate_all_dataframes_in_list(list_of_dataframe_labels)
+    predicted_columns=['prediction_'+x for x in label_types]
+    mse=mean_squared_error(concatenated_labels[label_types], concatenated_labels[predicted_columns])
+    return mse
 
 
 def preprocess_data(data):
@@ -51,7 +98,7 @@ def preprocess_data(data):
     return data
 
 
-def preprocess_data_and_labels_for_train(data, labels, label_type, AffectNet_embeddings_output_shape):
+def preprocess_data_and_labels_for_train(data, labels, label_type):
     mask_for_data=np.ones(shape=(data.shape[0], data.shape[1],AffectNet_embeddings_output_shape))
     data = preprocess_data(data)
     result_labels = np.zeros(shape=data.shape[:2] + (len(label_type),))
@@ -96,6 +143,7 @@ if __name__ == "__main__":
     model=create_sequence_model(input_shape, path_to_weights_AffectNet)
     model.compile(optimizer='Adam', loss='mse')
     print(model.summary())
+    evaluate_mse_on_database(path_RECOLA, model, ['arousal'])
     # train process
     for epoch in range(epochs):
         train_gen=train_generator(train_paths)
