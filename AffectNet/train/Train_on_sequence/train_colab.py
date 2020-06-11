fd2a6fbb0f71301ed693a49472e5b5c76658fb28
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2020-06-11 16:55:35 +0200

    function to evaluate Concordance correlation coefficient of database was added.

--- AffectNet/train/Train_on_sequence/train_colab.py
+++ AffectNet/train/Train_on_sequence/train_colab.py
@@ -2,7 +2,8 @@ import gc
 import os
 import pandas as pd
 import numpy as np
-from keras import backend as K
+import tensorflow as tf
+from tensorflow.keras import backend as K
 from sklearn.metrics import mean_squared_error
 
 from AffectNet.train.Train_on_AffectNet.BigTransfet_Resnet.model import create_sequence_model
@@ -13,22 +14,51 @@ def CCC_loss_tf(y_true, y_pred):
     This function calculates loss based on concordance correlation coefficient of two series: 'ser1' and 'ser2'
     TensorFlow methods are used
     """
+    #tf.print('y_true_shape:',tf.shape(y_true))
+    #tf.print('y_pred_shape:',tf.shape(y_pred))
 
-    y_pred = K.squeeze(y_pred, axis=-1)
-    y_true = K.squeeze(y_true, axis=-1)
+    y_true_mean = K.mean(y_true, axis=-2, keepdims=True)
+    y_pred_mean = K.mean(y_pred, axis=-2, keepdims=True)
 
-    y_true_mean = K.mean(y_true, axis=-1, keepdims=True)
-    y_pred_mean = K.mean(y_pred, axis=-1, keepdims=True)
+    y_true_var = K.mean(K.square(y_true-y_true_mean), axis=-2, keepdims=True)
+    y_pred_var = K.mean(K.square(y_pred-y_pred_mean), axis=-2, keepdims=True)
 
-    y_true_var = K.mean(K.square(y_true-y_true_mean), axis=-1, keepdims=True)
-    y_pred_var = K.mean(K.square(y_pred-y_pred_mean), axis=-1, keepdims=True)
+    cov = K.mean((y_true-y_true_mean)*(y_pred-y_pred_mean), axis=-2, keepdims=True)
 
-    cov = (y_true-y_true_mean)*(y_pred-y_pred_mean)
-
-    ccc = K.constant(2.) * cov / (y_true_var + y_pred_var + K.square(y_true_mean - y_pred_mean) + K.common.epsilon())
-    ccc_loss = K.constant(1.) - ccc
-
+    ccc = tf.math.multiply(2., cov) / (y_true_var + y_pred_var + K.square(y_true_mean - y_pred_mean) + K.epsilon())
+    ccc_loss=1.-K.mean(K.flatten(ccc))
+    #tf.print('ccc:', tf.shape(ccc_loss))
+    #tf.print('ccc_loss:',ccc_loss)
     return ccc_loss
+
+def CCC_batch_numpy(x, y):
+    x_mean=np.mean(x, axis=-2, keepdims=True)
+    y_mean = np.mean(y, axis=-2, keepdims=True)
+    x_var=np.var(x, axis=-2, keepdims=True)
+    y_var = np.var(y, axis=-2, keepdims=True)
+    covar=np.mean((x-x_mean)*(y-y_mean), axis=-2,keepdims=True)
+    result=2.*covar/(x_var+y_var+np.square(x_mean-y_mean))
+    #return np.mean(result.reshape((-1,)))
+    return result
+
+def CCC_2_sequences_numpy(y_true, y_pred):
+
+    cor = np.corrcoef(y_true, y_pred)[0][1]
+
+    mean_true = np.mean(y_true)
+    mean_pred = np.mean(y_pred)
+
+    var_true = np.var(y_true)
+    var_pred = np.var(y_pred)
+
+    sd_true = np.std(y_true)
+    sd_pred = np.std(y_pred)
+
+    numerator = 2 * cor * sd_true * sd_pred
+
+    denominator = var_true + var_pred + (mean_true - mean_pred) ** 2
+
+    return numerator / denominator
 
 def train_generator(paths, need_permutation=True):
     paths_to_data = np.array([], dtype='str')
@@ -70,27 +100,52 @@ def make_predictions_on_database(path_to_database, model, label_types):
     for mini_batch in gen:
         data, label = mini_batch
         data, mask_for_data, _ = preprocess_data_and_labels_for_train(data, label, label_types)
-        predicted_labels = model.predict([data, mask_for_data])
+        predicted_labels = model.predict([data, mask_for_data], batch_size=1)
         for idx_window in range(predicted_labels.shape[0]):
             for i in range(len(labels_type)):
-                label[idx_window]['prediction_' + labels_type[i]] = predicted_labels[i]
+                label[idx_window]['prediction_' + labels_type[i]] = predicted_labels[idx_window,:,i]
         real_labels += label
     return real_labels
 
 def concatenate_all_dataframes_in_list(list_of_dataframes):
     full_dataframe=list_of_dataframes[0]
     for i in range(1, len(list_of_dataframes)):
-        full_dataframe=pd.concat([full_dataframe, list_of_dataframes[i]])
+        full_dataframe=pd.concat([full_dataframe, list_of_dataframes[i]], axis=0)
     return full_dataframe
 
+def get_concatenated_predictions_for_database(path_to_database, model, label_types):
+    list_of_dataframe_labels = make_predictions_on_database(path_to_database, model, label_types)
+    concatenated_labels = concatenate_all_dataframes_in_list(list_of_dataframe_labels)
+    return concatenated_labels
 
-def evaluate_mse_on_database(path_to_database, model, label_types):
-    list_of_dataframe_labels=make_predictions_on_database(path_to_database, model, label_types)
-    concatenated_labels=concatenate_all_dataframes_in_list(list_of_dataframe_labels)
+def evaluate_mse_on_database(labels_and_predictions, label_types):
     predicted_columns=['prediction_'+x for x in label_types]
-    mse=mean_squared_error(concatenated_labels[label_types], concatenated_labels[predicted_columns])
+    mse=mean_squared_error(labels_and_predictions[label_types], labels_and_predictions[predicted_columns])
     return mse
 
+def evaluate_CCC_on_database(labels_and_predictions, label_types):
+    predicted_columns=['prediction_'+x for x in label_types]
+    labels_and_predictions['frame']=labels_and_predictions['frame'].apply(lambda x: x[:x.rfind('\\')])
+    grouped=labels_and_predictions.groupby(['frame', 'timestep']).mean()
+    # reset index to remove multiIndex
+    grouped=grouped.reset_index()
+    # delete frames with no face
+    grouped=grouped[grouped['frame']!='NO_FAC'] # do not ask me why NO_FAC
+    # spliting procedure
+    splited_dataframes=[]
+    unique_videofile_paths=np.unique(grouped['frame'])
+    for video_filename in unique_videofile_paths:
+        splited_dataframes.append(grouped[grouped['frame']==video_filename].sort_values(by=['timestep']))
+    # calculate weights
+    weights_for_CCC=np.array([x.shape[0] for x in splited_dataframes])
+    weights_for_CCC=weights_for_CCC/weights_for_CCC.sum()
+    # calculate CCC
+    CCC=np.zeros((len(label_types),))
+    for idx_dataframe in range(len(splited_dataframes)):
+        for idx_label in range(len(label_types)):
+            CCC[idx_label]+=weights_for_CCC[idx_dataframe]*CCC_2_sequences_numpy(splited_dataframes[idx_dataframe][label_types[idx_label]],
+                                                                                 splited_dataframes[idx_dataframe][predicted_columns[idx_label]])
+    return CCC
 
 def preprocess_data(data):
     data = data.astype('float32')
@@ -131,9 +186,9 @@ if __name__ == "__main__":
     channels = 3
     image_shape = (width, height, channels)
     input_shape = (sequence_length, width, height, channels)
-    labels_type = ['arousal']
+    labels_type = ['arousal', 'valence']
     epochs = 10
-    batch_size = 1
+    batch_size = 2
     verbose = 1
 
     train_loss=[]
@@ -141,15 +196,17 @@ if __name__ == "__main__":
     # model
     path_to_weights_AffectNet = 'C:\\Users\\Dresvyanskiy\\Downloads\\weights_arousal_valence.h5'
     model=create_sequence_model(input_shape, path_to_weights_AffectNet)
-    model.compile(optimizer='Adam', loss='mse')
+    model.compile(optimizer='Adam', loss=CCC_loss_tf)
     print(model.summary())
-    evaluate_mse_on_database(path_RECOLA, model, ['arousal'])
+    #evaluate_mse_on_database(path_RECOLA, model, ['arousal'])
+    labels_and_predictions=get_concatenated_predictions_for_database(path_RECOLA, model, labels_type)
+    res=evaluate_CCC_on_database(labels_and_predictions, labels_type)
     # train process
     for epoch in range(epochs):
         train_gen=train_generator(train_paths)
         for batch in train_gen:
             train_data, train_labels=batch
-            train_data, train_mask, train_labels=preprocess_data_and_labels_for_train(train_data, train_labels, labels_type, AffectNet_embeddings_output_shape)
+            train_data, train_mask, train_labels=preprocess_data_and_labels_for_train(train_data, train_labels, labels_type)
             for mini_batch_idx in range(0, train_data.shape[0], batch_size):
                 start=mini_batch_idx
                 end=mini_batch_idx+batch_size
